d99927c25956b7d5918a09b9daf21aa89cbf4b1c
Michael Gardner <gardnmi@gmail.com>
2025-06-20 04:14:55 +0000

    updated to use unfold

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Profile
+from accounts.models import Profile
+
+from unfold.admin import ModelAdmin
+
+
+admin.site.unregister(Profile)  # Unregister the default Profile admin if it exists
 
 # Register your models here.
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(ModelAdmin):
     list_display = ('user', 'location', 'birth_date')
     search_fields = ('user__username', 'location')
